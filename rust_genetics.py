6a70d2479925be28d6596f8ea949c425c547c498
ngupt20 <ngupta20@github.com>
2022-05-02 21:35:18 -0400

    opt

--- rust_genetics.py
+++ rust_genetics.py
@@ -1,25 +1,32 @@
 from itertools import combinations
+from typing import List, Tuple
 
 import numpy as np
 
+WEIGHT = [0.6, 0.6, 0.6, 1.0, 1.0]
 POSSIBLE = tuple("YGHXW")
 GREEN = tuple("YGH")
 RED = tuple("XW")
-WEIGHT = [0.6, 0.6, 0.6, 1.0, 1.0]
 
 
-def recommend(file: str = "genes.csv"):
-    with open(file, "r") as file:
-        gene_pool = np.array(tuple(map(tuple, file.read().split("\n"))))
-    cs = [
-        gene_pool[np.array(tuple(combinations(range(len(gene_pool)), i)))]
-        for i in range(1, 9)
-    ]
-    ws = [(cs[i][:, :, :, None] == POSSIBLE).sum(axis=1) * WEIGHT for i in range(8)]
-    vs = [
-        cs[i][
-            (ws[i].max(axis=2)[:, :, None] == ws[i])[:, :, :3].any(axis=2).all(axis=1)
-        ]
-        for i in range(8)
-    ]
-    return sum([[["".join(z) for z in y] for y in x] for x in vs if x.shape[0] > 0], [])
+def recommendations(gene_pool: List[str]) -> Tuple[List[np.ndarray], List[np.ndarray]]:
+    pool = np.array(tuple(map(tuple, gene_pool)))
+    n = min(len(pool) + 1, 9)
+
+    viable_combinations = []
+    viable_outcomes = []
+    for i in range(n - 1):
+        index_combinations = np.array(tuple(combinations(range(len(pool)), i + 1)))
+        possible = pool[index_combinations]
+        weights = (possible[:, :, :, None] == POSSIBLE).sum(axis=1) * WEIGHT
+        outcomes = (weights.max(axis=2)[:, :, None] == weights)[:, :, :2]
+        viable = (
+            ((outcomes[:, :, [0]] ^ outcomes[:, :, [1]]) & outcomes[:, :, :2]).sum(
+                axis=1
+            )
+            >= 3
+        ).all(axis=1)
+        viable_combinations += [possible[viable]]
+        viable_outcomes += [outcomes[viable]]
+
+    return viable_combinations, viable_outcomes
