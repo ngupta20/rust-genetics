b9689f02c43c5a693daffcbb454fb4d10ef2682c
ngupt20 <ngupta20@github.com>
2022-05-07 23:10:39 -0400

    updte

--- rust_genetics.py
+++ rust_genetics.py
@@ -9,23 +9,28 @@ GREEN = tuple("YGH")
 RED = tuple("XW")
 
 
-def recommendations(gene_pool: List[str]) -> Tuple[List[np.ndarray], List[np.ndarray]]:
+def recommendations(
+    gene_pool: List[str], viability_method="yg"
+) -> Tuple[List[np.ndarray], List[np.ndarray]]:
     pool = np.array(tuple(map(tuple, gene_pool)))
+    pool = pool[(pool[:, :, None] == RED).any(axis=2).sum(axis=1) <= 2]
     n = min(len(pool) + 1, 9)
-
     viable_combinations = []
     viable_outcomes = []
     for i in range(n - 1):
         index_combinations = np.array(tuple(combinations(range(len(pool)), i + 1)))
         possible = pool[index_combinations]
         weights = (possible[:, :, :, None] == POSSIBLE).sum(axis=1) * WEIGHT
-        outcomes = (weights.max(axis=2)[:, :, None] == weights)[:, :, :2]
-        viable = (
-            ((outcomes[:, :, [0]] ^ outcomes[:, :, [1]]) & outcomes[:, :, :2]).sum(
-                axis=1
-            )
-            >= 3
-        ).all(axis=1)
+        outcomes = weights.max(axis=2)[:, :, None] == weights
+        if viability_method == "yg":
+            viable = outcomes[:, :, :2].any(axis=2).sum(axis=1) >= 6
+        elif viability_method == "all_green":
+            viable = outcomes[:, :, :3].any(axis=2).sum(axis=1) >= 6
+        elif viability_method == "yg-1":
+            viable = outcomes[:, :, :3].any(axis=2).sum(axis=1) >= 5
+        else:
+            raise NotImplementedError()
+        print(f"Combination Size: {i + 1}    Viable: {viable.sum()}")
         viable_combinations += [possible[viable]]
         viable_outcomes += [outcomes[viable]]
 
